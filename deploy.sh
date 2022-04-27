8c3ccf2532a95e055e8d623f2fa56a1073811a12
nick-cheatwood7 <nick.cheatwood@gmail.com>
2022-04-27 08:20:08 -0500

    cleaned up codebase

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ read VERSION
 echo Enter Docker username:
 read USER
 
-docker build -t $USER/node-express-template:$VERSION .
-docker push $USER/node-express-template:$VERSION
+docker build -t $USER/ts-graphql-template:$VERSION .
+docker push $USER/ts-graphql-template:$VERSION
 
 # Deploy on server (replace `root@127.0.0.1` with VPS config)
-ssh root@127.0.0.1 "docker pull $USER/node-express-template:$VERSION && docker tag $USER/node-express-template:$VERSION dokku/api:$VERSION && dokku deploy api $VERSION"
+ssh root@127.0.0.1 "docker pull $USER/ts-graphql-template:$VERSION && docker tag $USER/ts-graphql-template:$VERSION dokku/api:$VERSION && dokku deploy api $VERSION"
